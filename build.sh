3c624f4512b5b91692be337e67ff385e46f17581
suyuan168 <175338101@qq.com>
2022-05-09 17:43:05 +0800

    fix

--- build.sh
+++ build.sh
@@ -5,14 +5,27 @@ rm -rf package/kernel/mac80211
 
 btime=`date +%Y-%m-%d`
 case $1 in
-     x5)
+x5)
+echo "开始下载dl文件"
+#如果文件不存在，则创建文件
+tempFile="dl.zip"
+if [ ! -f "$tempFile" ]; then
+wget http://55860.com/bak/dl.zip
+unzip -q -o dl.zip
+fi
+echo "开始编译"
+
+echo "开始编译qsdk 5.4 ipq6x咯"
       #   tar zxvf package/kernel/mac80211.orig.tar.gz -C package/kernel/
         tar zxvf package/kernel/mac80211.ipq60xx.tar.gz -C package/kernel/
         rm -rf files
+scripts/feeds clean
+scripts/feeds update -a
+scripts/feeds install -a
         cp ylx_files/x5/x5.config .config
-        cp ylx_files/x5/files/ . -rf
-        cp logo/logo_linux_clut224.ppm.5g build_dir/target-aarch64_cortex-a53_musl/linux-ipq60xx_generic/linux-5.4.164/drivers/video/logo/logo_linux_clut224.ppm
+        
         make V=s $2
+        echo "编译完完成"
         cp bin/targets/ipq60xx/generic/x5-21.02-v1.0-ipq60xx-generic-ylx_x5-squashfs-nand-factory.ubi firm/$btime-x5.bin
         ;;
       x2)
