cc0fb1eac79f9d17e757d7a8de26419e1c3f9a6c
suyuan168 <175338101@qq.com>
2022-05-09 18:06:00 +0800

    Update generic.mk

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -1,20 +1,20 @@
 define Device/FitImage
-  KERNEL_SUFFIX := -fit-uImage.itb
-  KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-  KERNEL_NAME := Image
+	KERNEL_SUFFIX := -fit-uImage.itb
+	KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
 endef
 
 define Device/FitImageLzma
-  KERNEL_SUFFIX := -fit-uImage.itb
-  KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-  KERNEL_NAME := Image
+	KERNEL_SUFFIX := -fit-uImage.itb
+	KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
 endef
 
 define Device/UbiFit
-  KERNEL_IN_UBI := 1
-  IMAGES := nand-factory.ubi nand-sysupgrade.bin
-  IMAGE/nand-factory.ubi := append-ubi
-  IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
+	KERNEL_IN_UBI := 1
+	IMAGES := nand-factory.ubi nand-sysupgrade.bin
+	IMAGE/nand-factory.ubi := append-ubi
+	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 
 define Device/ylx_y6000
